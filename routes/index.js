af8f9961c65e3230cf13e71dc492e5b833f8f2b1
Sheila Nasehi (BDA/Outsourcing/Shared Svcs <sheila.nasehi@towerswatson.com>
2018-04-11 20:09:29 -0700

    411 version

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,13 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-router.get('/test', function (req, res) {
-    res.send("This is a test");
-});
+//anything related to data routes goes here 
 
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  //anything related to the react app goes in here will go to react route 
 });
 
 module.exports = router;
